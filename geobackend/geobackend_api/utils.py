applied session-based calculation result storage and fixed issues with caching WMS queries

geobackend/geobackend_api/utils.py
@@ -1,30 +1,41 @@
+from django.core.serializers.json import DjangoJSONEncoder
+
 from pyproj import Transformer
 import numpy as np
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import urllib
 import re
+
 import logging
 from .reference_data import *
 
 # caching
-import time, requests, redis, pickle
+import requests
+import redis
+import pickle
 from hashlib import md5
 
-# Connect to Redis
+
+# global variables
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
-
-def generate_cache_key(params):
-    # Generate a unique cache key based on the request parameters
-    key_string = str(params)
-    return md5(key_string.encode('utf-8')).hexdigest()
-
-
+redis_timeout = 3600*24 #1 day
 logger = logging.getLogger('geobackend_api')
 
+class GeoDjangoJSONEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, pd.DataFrame):
+            return obj.to_dict(orient='records')
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, np.generic):
+            return np.asscalar(obj)
+        return super().default(obj)
 
-def process_depth_data(coordinates, min_resolution:int|float=100, pixels=(100,100), crs_type:str='wgs84'):
-    bbox_params = get_bbox_params(coordinates, min_resolution, pixels, crs_type)
+def process_depth_data(coordinates, min_resolution: int | float = 100, pixels=(100, 100), crs_type: str = 'wgs84'):
+    bbox_params = get_bbox_params(
+        coordinates, min_resolution, pixels, crs_type)
     layer_request = wms_layer_request(bbox_params)
     layers = parse_wms_layers(layer_request)
     layers_as_string = stringify_layers(layers)
@@ -33,11 +44,12 @@ def process_depth_data(coordinates, min_resolution:int|float=100, pixels=(100,10
     formatted_depth_data = format_data_depth_table(layer_data)
     return formatted_depth_data
 
-def get_bbox_params(coordinates, min_resolution:int|float=100, pixels=(100,100), crs_type:str='wgs84' ):
+
+def get_bbox_params(coordinates, min_resolution: int | float = 100, pixels=(100, 100), crs_type: str = 'wgs84'):
     """
     transforms provided coordinates into web mercator coordinate system
     and returns the bbox range as a string
-    
+
     inputs: coorindates:tuple or list, 
     crs_type(string), 
     minimum resolution of the queried data in metre
@@ -45,10 +57,12 @@ def get_bbox_params(coordinates, min_resolution:int|float=100, pixels=(100,100),
     output: 
     f'{bbox_xy}'.strip('()')
     """
-    x, y = Transformer.from_crs(crs_type, 'epsg:3857').transform(*coordinates) #EPSG:3857 for mercator
+    x, y = Transformer.from_crs(crs_type, 'epsg:3857').transform(
+        *coordinates)  # EPSG:3857 for mercator
     incre = np.array(pixels) * min_resolution / 2
-    bbox_xy =x-incre[0], y-incre[1], x+incre[0], y+incre[1]
+    bbox_xy = x-incre[0], y-incre[1], x+incre[0], y+incre[1]
     return f'{bbox_xy}'.strip('()'), *_get_bbox_pixel_params(pixels)
+
 
 def _get_bbox_pixel_params(pixels):
     """
@@ -56,6 +70,27 @@ def _get_bbox_pixel_params(pixels):
     returns width, height, x, y = get_bbox_params(pixels)
     """
     return *pixels, int(pixels[0] / 2), int(pixels[1] / 2)
+
+
+def generate_cache_key(params):
+    # Generate a unique cache key based on the request parameters
+    key_string = str(params)
+    return md5(key_string.encode('utf-8')).hexdigest()
+
+
+def load_or_get_results(url, params):
+    cache_key = generate_cache_key(params)
+    cached_result = redis_client.get(cache_key)
+    if cached_result:
+        logger.info(f'cache hit for {cache_key}')
+        return pickle.loads(cached_result)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        redis_client.setex(cache_key, redis_timeout, pickle.dumps(response))
+        return response
+    except requests.exceptions.RequestException as e:
+        logger.error(e)
 
 
 def wms_layer_request(bbox_params):
@@ -83,42 +118,47 @@ def wms_layer_request(bbox_params):
     }
 
     url = f"{base_url}?{urllib.parse.urlencode(params)}"
-    #caching
-    cache_key = generate_cache_key(params)
+    # caching
+    return load_or_get_results(url, params)
+    # cache_key = generate_cache_key(params)
 
-    cached_result = redis_client.get(cache_key)
-    if cached_result:
-        return pickle.loads(cached_result) 
-    
-    try:
-        response = requests.get(url)    
-        response.raise_for_status()
-        redis_client.setex(cache_key, 3600, pickle.dumps(response))
-        return response
-    except requests.exceptions.RequestException as e:
-        logger.error(e)
-        
+    # cached_result = redis_client.get(cache_key)
+    # if cached_result:
+    #     return pickle.loads(cached_result)
 
-def parse_wms_layers(response:requests.Response) -> list:
+    # try:
+    #     response = requests.get(url)
+    #     response.raise_for_status()
+    #     redis_client.setex(cache_key, 3600, pickle.dumps(response))
+    #     return response
+    # except requests.exceptions.RequestException as e:
+    #     logger.error(e)
+
+
+
+def parse_wms_layers(response: requests.Response) -> list:
     soup = BeautifulSoup(response.text, 'html.parser')
     if response.status_code == 200:
-        layers = sorted([code.text.lower() for code in soup.find_all('div',class_='aquifer-id')])
-        layers += ['114bse'] #basement layer at the bottom
+        layers = sorted([code.text.lower()
+                        for code in soup.find_all('div', class_='aquifer-id')])
+        layers += ['114bse']  # basement layer at the bottom
         return layers
     else:
-        logger.error(f"Error in parse_wms_layers: {response.status_code}, {response.text}")
-        raise requests.exceptions.HTTPError(f"Error: {response.status_code}, {response.text}")
-    
+        logger.error(f"Error in parse_wms_layers: {
+                     response.status_code}, {response.text}")
+        raise requests.exceptions.HTTPError(
+            f"Error: {response.status_code}, {response.text}")
 
-def stringify_layers(layers:list):
+
+def stringify_layers(layers: list):
     prefix, suffix = 'vaf_', '_group'
     keys = list(vaf_mapping)
     keys.pop(14)
-    layers_as_string = ",".join([ prefix+layer+suffix for layer in layers])
+    layers_as_string = ",".join([prefix+layer+suffix for layer in layers])
     return layers_as_string
 
 
-def wms_aquifer_info_request(layer_string:str, bbox_params):
+def wms_aquifer_info_request(layer_string: str, bbox_params):
     """
     layer_string: stringified representation of the requested layers
     bbox_params: same params as wms_layer_request
@@ -143,37 +183,27 @@ def wms_aquifer_info_request(layer_string:str, bbox_params):
         "FEATURE_COUNT": 50
     }
     url = f"{base_url}?{urllib.parse.urlencode(params)}"
-    #caching
-    cache_key = generate_cache_key(params)
-
-    cached_result = redis_client.get(cache_key)
-    if cached_result:
-        return pickle.loads(cached_result) 
-    
-    try:
-        response = requests.get(url)    
-        response.raise_for_status()
-        redis_client.setex(cache_key, 3600, pickle.dumps(response))
-        return response
-    except requests.exceptions.RequestException as e:
-        logger.error(e)
+    # caching
+    return load_or_get_results(url, params)
 
 
 def parse_layer_info(response):
     if response.status_code != 200:
-        logger.error(f"Error in parse_layer_info: {response.status_code}, {response.text}")
-        raise requests.exceptions.HTTPError(f"Error: {response.status_code}, {response.text}")
-    
+        logger.error(f"Error in parse_layer_info: {
+                     response.status_code}, {response.text}")
+        raise requests.exceptions.HTTPError(
+            f"Error: {response.status_code}, {response.text}")
+
     data = {}
     soup = BeautifulSoup(response.text, 'html.parser')
     rows = soup.find_all('div', class_='row')
-    
+
     for row in rows:
         cols = row.find_all('div')
         if len(cols) == 2:
             key = cols[0].text.strip()
             value = cols[1].text.strip()
-            
+
             match = re.match(r"(\w+)\s+(\d+)", key)
             if match:
                 field, num_part = match.groups()
@@ -185,42 +215,44 @@ def parse_layer_info(response):
                         # Handle the -9999 value
                         float_value = float(value.replace(',', ''))
                         if float_value == -9999:
-                            logger.info(f"Aqdepth for {layer_code} is -9999, treating as 0")
+                            logger.info(f"Aqdepth for {
+                                        layer_code} is -9999, treating as 0")
                             float_value = 0
                         data[layer_code][field] = float_value
                     except ValueError:
-                        logger.error(f"Could not convert {value} to float for {key}")
+                        logger.error(f"Could not convert {
+                                     value} to float for {key}")
     return data
 
 
-def format_data_depth_table(layer_data:list):
-    layer_dict = {'aquifer_layer':[],
-                "is_aquifer":[],
-                "depth_to_base":[]}
+def format_data_depth_table(layer_data: list):
+    layer_dict = {'aquifer_layer': [],
+                  "is_aquifer": [],
+                  "depth_to_base": []}
     for key, item in layer_data.items():
         layer_dict['aquifer_layer'].append(key)
         layer_dict['is_aquifer'].append(is_aquifer[key])
         aquidepth = item.get('Aqdepth', 0)
-        if aquidepth == -9999: aquidepth = 0 #ensure -9999 is treated as 0
-        thickness = item.get('Thickness',0)
+        if aquidepth == -9999:
+            aquidepth = 0  # ensure -9999 is treated as 0
+        thickness = 200 if 'bse' in key else item.get('Thickness', 0) #basement thickness fixed at 200
         depth = aquidepth + thickness
         layer_dict['depth_to_base'].append(depth)
     return layer_dict
 
 
 def check_feasibility(layer_dict):
-    #TODO: implement check
-    layers = layer_dict['aquifer_layer'] 
+    # TODO: implement check
+    layers = layer_dict['aquifer_layer']
     if len(layers) == 0:
         return False, {'message': 'Aquifer layer is empty'}
     top_layer = layers[0]
     if top_layer not in ['100qa', '102utqa']:
         return False, {'message': 'Top layer is not aquifer'}
-    #TODO: change this logic later
-    target_layer = next((layer for layer in ('109lmta','111lta') if layer in layers),
+    # TODO: change this logic later
+    target_layer = next((layer for layer in ('109lmta', '111lta') if layer in layers),
                         False)
     if not target_layer:
         return False, {'message': 'Target layers not present in the layer list'}
-    return True, {'top_aquifer_layer':top_layer, 'target_aquifer_layer':target_layer}
-    
+    return True, {'top_aquifer_layer': top_layer, 'target_aquifer_layer': target_layer}
 

geobackend/geobackend_api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-#DRF
+# DRF
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
@@ -9,7 +9,13 @@ import pandas as pd
 
 from .models import *
 from .serializers import *
-from .utils import process_depth_data, check_feasibility
+from .utils import process_depth_data, check_feasibility, GeoDjangoJSONEncoder, generate_cache_key
+
+import json
+import logging
+
+
+logger = logging.getLogger('geobackend_api')
 
 
 class WellBoreCalcView(APIView):
@@ -28,26 +34,27 @@ class WellBoreCalcView(APIView):
             initial_input_values = validated_data['initial_input_values']
             is_production_pump = validated_data['is_production_pump']
 
-            #query WMS data
+            # query WMS data
             depth_data = process_depth_data(coodinates,
                                             min_resolution,
                                             pixels,
                                             crs_type)
-            
-            #check if the calculation can be performed on the queried data
+
+            # check if the calculation can be performed on the queried data
             is_feasible, response_feasible = check_feasibility(depth_data)
             if not is_feasible:
                 return Response(response_feasible,
                                 status=status.HTTP_400_BAD_REQUEST)
-            
-            #update and validate initial_input_values object
+
+            # update and validate initial_input_values object
             initial_input_values['top_aquifer_layer'] = response_feasible['top_aquifer_layer']
             initial_input_values['target_aquifer_layer'] = response_feasible['target_aquifer_layer']
 
-            input_serializer = InitialInputSerializer(data=initial_input_values)
+            input_serializer = InitialInputSerializer(
+                data=initial_input_values)
             if not input_serializer.is_valid():
                 return Response(input_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
+
             # convert depth data to pandas dataframe
             depth_data_df = pd.DataFrame(depth_data)
 
@@ -59,23 +66,26 @@ class WellBoreCalcView(APIView):
                 initial_input_data=initial_input_values
             )
             results = geo_interface.export_results_to_dict()
+            json_results = json.dumps(results, cls=GeoDjangoJSONEncoder)
+
 
             # Save the results to the model
             session_key = request.session.session_key
-            WellBoreCalculationResult.objects.create(session_key=session_key, result_data=results)
+            _, created = WellBoreCalculationResult.objects.update_or_create(
+                session_key=session_key, result_data=json_results)
+            logging.info(f"{'Created' if created else 'Updated'} WellBoreCalculationResult results for {session_key}")
 
-        
-            return Response({'message':'Calculation successful', 
+            return Response({'message': 'Calculation successful',
                              'data': results}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-
 class TestWellboreCalculationView(APIView):
     def post(self, request, *args, **kwargs):
         data = request.data
+        if not request.session.session_key:
+            request.session.create()
 
         # extract parameters from request
         is_production_pump = data["is_production_pump"]
@@ -85,10 +95,9 @@ class TestWellboreCalculationView(APIView):
         # convert depth data to pandas dataframe
         depth_data_df = pd.DataFrame(depth_data)
 
-
         # initialize and use the calculation object
         geo_interface = gdc.GeoDrillCalcInterface()
-        result_wbd = geo_interface.calculate_and_return_wellbore_parameters(
+        geo_interface.calculate_and_return_wellbore_parameters(
             is_production_pump=is_production_pump,
             depth_data=depth_data_df,
             initial_input_data=initial_input_values
@@ -96,16 +105,14 @@ class TestWellboreCalculationView(APIView):
 
         # #retrieve the result as dictionary
         results = geo_interface.export_results_to_dict()
-        #print(results)
-        #results = results.replace(np.nan, None) #np.nan not serializable
-        #results = results.to_json()
-        # #store the results in the database
-        # depth_query = DepthQuery(results)
-        # depth_query.save()
+        json_results = json.dumps(results, cls=GeoDjangoJSONEncoder)
+        # Save the results to the model
 
-        # #serialize and return the results
-        # serializer = DepthQuerySerializer(depth_query)
-        #print(results)
+
+        session_key = request.session.session_key
+        _, created = WellBoreCalculationResult.objects.update_or_create(
+            session_key=session_key, result_data=json_results)
+        logging.info(f"{'Created' if created else 'Updated'} WellBoreCalculationResult results for {session_key}")
         
-        return Response({'data':results}, status=status.HTTP_200_OK)
-    
+
+        return Response({'data': results}, status=status.HTTP_200_OK)