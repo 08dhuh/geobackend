7b1a5a5f3e61be979f249fcae076a4465c3e14d1
Doran Huh <08dhuh@gmail.com>
2024-08-30 15:09:38 +1000

    correct import statement

--- geobackend/geobackend_api/views.py
+++ geobackend/geobackend_api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
-from geodrillcalc import geodrillcalc as gdc
+import geodrillcalc.geodrillcalc_interface as gdc
 import pandas as pd
 
 from .models import *
